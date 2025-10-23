b39263c9730ff4ee374bdff325d06f0b427d5ee8
NoSoyVerde <hectorkotor@gmail.com>
2025-10-23 12:02:01 +0200

    Añadida ruta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Landing } from './components/landing/landing';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Landing
+    },
+    {
+        path: 'home',
+        component: Home
+    }
+];
